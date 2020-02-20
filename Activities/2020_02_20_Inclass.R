f8f0c12126c56458df347ee865d77545d8491c1f
plutzer <41806910+plutzer@users.noreply.github.com>
2020-02-20 12:36:20 -0600

    Added the feb 20 activity

---
+++ Activities/2020_02_20_Inclass.R
@@ -0,0 +1,32 @@
+rm(list = ls())
+library(tidyverse)
+
+mayors<-read_csv(file="https://raw.githubusercontent.com/jmontgomery/jmontgomery.github.io/master/PDS/Datasets/Mayors.csv")
+tweets<-read_csv("~/PDS/Tweets.csv")
+
+#LydaKrewson
+lydatweets = tweets %>%
+  filter(ScreenName == "lydakrewson")
+
+#Number of tweets
+num_tweets = length(str_split(lydatweets$Text, pattern  = " "))
+
+#Mean number of words
+mean_words = length(unlist(str_split(lydatweets$Text, pattern  = " ")))/num_tweets[1]
+
+#Number of unique words
+num_unique_words = length(unique(unlist(str_split(lydatweets$Text, pattern  = " "))))
+
+#First five characters of unique words
+unique_words = unique(unlist(str_split(lydatweets$Text, pattern  = " ")))
+sub_words = str_sub(unique_words,1,5)
+length(unique(sub_words))
+
+#Number of tweets with 'polic'
+sum(str_detect(str_to_lower(lydatweets$Text),pattern = 'polic'))
+
+#Number of tweets with 'police'
+sum(str_detect(str_to_lower(lydatweets$Text),pattern = 'police'))
+
+#Number of tweets with a link
+sum(str_detect(str_to_lower(lydatweets$Text),pattern = 'http'))
