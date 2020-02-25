96b2cf7f1fe4eab77234981c904256cbb49ac248
ilee3949 <ilee3949@users.noreply.github.com>
2020-02-25 11:21:29 -0600

    Including 02/25 Activity
    hi

---
+++ Activities/2020_02_25_InClass.R
@@ -0,0 +1,28 @@
+###Group Assignment 2
+#Using the tools from this lecture:
+#Go to the variables that start with inst, which represent the level of trust that Americans have in different insitutions.
+#Write a simple set of code to recode these variables to accurately reflect NAs
+#Find the mean level of support for each insitutution
+#Find the median level of support for each institution
+#Make a plot showing trust in institutions ordering institutiosn from least trusted to most trusted among the American public
+
+rm(list=ls())
+
+library(tidyverse)
+VSG<-read_csv("~/Downloads/VOTER_Survey_Jan217_Release1-csv.csv")
+
+VSG<- VSG %>% 
+  select(starts_with("inst_")) %>% 
+
+  #Similar code from Class
+  #map(VSG.fav, mean, na.rm=TRUE)
+    #VSG<- VSG %>% 
+     # select(fav_sanders_2019, fav_biden_2019) %>%
+     # mutate(fav_sanders_binary= na_if(fav_sanders_2019, fav_sanders_2019>4)) %>%
+     # mutate(fav_sanders_binary = (fav_sanders_2019<=2)*1) %>%
+     # mutate(fav_biden_binary= na_if(fav_biden_2019, fav_biden_2019>4)) %>%
+     # mutate(fav_biden_binary = (fav_biden_2019<=2)*1)
+   # library(magrittr)
+    #VSG %$% 
+
+
