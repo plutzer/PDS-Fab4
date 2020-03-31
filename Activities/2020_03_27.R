44fc80738fc4cab36b75f1ef2496f14f6502c24c
ilee3949 <ilee3949@users.noreply.github.com>
2020-03-30 19:08:49 -0500

    Create 2020_03_27.R

---
+++ Activities/2020_03_27.R
@@ -0,0 +1,10 @@
+#Group Assignment
+#For Class-Discussion-03-27
+
+#Group assignment
+#Make your own model to predict senate races.
+#How does your model do in an out-of sample test compared to my simple model above? Use RMSE. And remember that smaller values of RMSE are better.
+#Now go get the data at this link and use it to make predictions for 2018 (you will need to change some of the variable names)
+#http://politicaldatascience.com/PDS/Datasets/SenateForecast/CandidateLevel2018.csv
+#Use this data to make predicitons for 2018. Add into slack your predictions for: 2018MOMcCaskill, 2018OHBrown, and 2018WVManchin. Please put your predictions for these races up BEFORE the next scheduled class session.
+
